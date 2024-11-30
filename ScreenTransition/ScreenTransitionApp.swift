4baf91e501fbb038c827a668badc4026d5510ffb
silicagel <ksk19981004@gmail.com>
2024-11-30 19:04:59 +0900

    Initial Commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by silicagel on 2024/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
